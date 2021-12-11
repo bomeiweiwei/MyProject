10ec116f2847a2d19d0b78365b9fdc2064052335
DESKTOP-MYAMD\bomei <bomeilonch0210@gmail.com>
2021-12-11 11:11:59 +0800

    調整Program AddMVC，加入用戶端程式庫bootstrap jquery，加入layout，測試Controller

--- slnAllShow/prjAllShow.Backend/Program.cs
+++ slnAllShow/prjAllShow.Backend/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddMvc();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
