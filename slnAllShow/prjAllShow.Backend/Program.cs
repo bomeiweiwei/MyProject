7037b70570cf281c5c02702e713239995108f578
DESKTOP-MYAMD\bomei <bomeilonch0210@gmail.com>
2021-12-11 12:43:12 +0800

    參考網址1.在現有的C# mvc 專案導入Vue.js ，引入Vue

--- slnAllShow/prjAllShow.Backend/Program.cs
+++ slnAllShow/prjAllShow.Backend/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
@@ -6,6 +8,17 @@ builder.Services.AddMvc();
 var app = builder.Build();
 
 app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Scripts")),
+    RequestPath = new PathString("/scripts")
+});
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "node_modules")),
+    RequestPath = new PathString("/vendor")
+});
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
---
+++ slnAllShow/prjAllShow.Backend/Scripts/Home/Index.js
@@ -0,0 +1,10 @@
+﻿new Vue({
+    el: '#app',
+    data: {
+        items: [
+            { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+            { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+            { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+            { age: 38, first_name: 'Jami', last_name: 'Carney' }]
+    }
+})
