3df4f90a2b6cebfb33b33a85d68245acde99bd60
DESKTOP-MYAMD\bomei <bomeilonch0210@gmail.com>
2021-12-31 22:36:51 +0800

    測試呼叫API專案

--- slnAllShow/prjAllShow.WebAPI/Program.cs
+++ slnAllShow/prjAllShow.WebAPI/Program.cs
@@ -1,5 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
-
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("http://localhost:5200");
+                          //.AllowAnyHeader()
+                          //.AllowAnyMethod(); 
+                      });
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -15,6 +25,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseRouting();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
