f9839262274e5d56e9445f74bb750cf7cebf43dc
鍾興偉 <bg0450@mail.taipei.gov.tw>
2022-02-10 10:42:02 +0800

    Partial新增

---
+++ slnAllShow/prjAllShow.Backend/Scripts/Admin/Employee/Index.js
@@ -0,0 +1,14 @@
+﻿$("[name='sapnCircle']").on('click', function () {
+    var num = $(this).find("span").text();
+    var searchStr = $("#txtSearchString").val();
+    location.href = encodeURI('/admin/Employee/Index?currentFilter=' + searchStr + '&pageNumber=' + num);
+});
+
+$("[name='sapnCircle'] span").each(function (index) {
+    //console.log( index + ": " + $( this ).text() );
+    //var num = $(this).find("span").text();
+    var num = $(this).text();
+    if (num == "@Model.PageIndex") {
+        $(this).css('color', '#2263B7');
+    }
+});
